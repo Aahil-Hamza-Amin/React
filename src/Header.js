1de4646cf858e86ed6673eecce87b13277f00c02
Aahil-Hamza-Amin <aahilhami@gmail.com>
2025-06-11 22:34:05 +0500

    Adding Styling with new index.css

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,10 @@
 function Header() {
+    // For Inline CSS or use {{}}
+ /*    const styleHeader = {
+        backgroundColor: "royalblue", color: "white"
+    }; */
     return (
-        <header>
+        <header /* style={styleHeader} */>
             <h1>Groceries List</h1>
         </header>
     )
