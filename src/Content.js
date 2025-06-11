9ffe6570436b9d716e42ddead32a236066aaca35
Aahil-Hamza-Amin <aahilhami@gmail.com>
2025-06-12 02:38:13 +0500

    Added useState Hook

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,28 @@
+import { useState } from "react";
+
 function Content() {
+    const [name, setName] = useState("Hamza");
+    const [count, setCount] = useState(0);
     const handleNameChange = () => {
         const names = ['Hamza', 'Ali', "Ahmad"];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        setName(names[int]);
     }
-    //Reason.... we pass refenrence b'cs unless Fn would get called immediately even we did't cliked the button(trigered event) 
-
-    // we pass the reference of Fn to event as yu can see on Event
     const handleClick = () => {
-        console.log('You clicked it!');
+        setCount(count + 1)
+        // setCount(count + 1)
+        console.log(count);
     }
-    // we pass anonymous Fn to event inside of that fn we call the referenced fn where we pass arguments 
-    const handleClick2 = (name) => {
-        console.log(`${name} has cliked it!`);
-    }
-    // we pass anonymous Fn to event inside of that fn we call the referenced fn where we pass event itself
-    const handleClick3 = (e) => {
-        console.log(e.target.innerText);
+    const handleClick2 = () => {
+        console.log(count);
     }
 
     return (
         <main>
-            {/* on doubleClick we've passed reference of Fn */}
-            <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
-            <button onClick={handleClick}>Click it</button> {/* referenced fn without  parameter*/}
-            <button onClick={() => handleClick2('Hamza')}>Click it</button> {/* referenced fn with parameter*/}
-            <button onClick={(e) => handleClick3(e)}>Click it</button> {/* referenced fn with passed event itself*/}
+            <p onDoubleClick={handleClick}>Hello {name}!</p>
+            <button onClick={handleNameChange}>Click it</button>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={handleClick2}>Click it</button>
         </main>
     )
 }
