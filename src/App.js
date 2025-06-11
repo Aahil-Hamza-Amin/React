3073c963d5d36048e5bf6561eb59dcdad15a604d
Aahil-Hamza-Amin <aahilhami@gmail.com>
2025-06-11 22:06:56 +0500

    Added Functional Components as Header, Content Footer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 function App() {
-
-  const name = 'Hamza';
-  const handleNameChange= () => {
-    const names = ['Hamza', 'Ali', "Ahmad"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to see changes.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello {name}</p>
-        <p>Hello {[1,2,3]}</p>{/* can pass array, number, str, not boolean or object */}
-        <p>Hello {handleNameChange()}!</p>
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    const today = new Date();
+    return (
+        <footer>
+            Copyright &copy; {today.getFullYear()}
+        </footer>
+    )
+}
+
+export default Footer
